01f12becf3cdb33ebe6feed7ba3357ead471b02a
Gayle Ngozi <gaylengozi@gmail.com>
2023-07-06 12:15:53 +0100

    changed background color

---
+++ client/src/pages/main-page/MainPage.js
@@ -0,0 +1,15 @@
+import "../main-page/MainPage.css";
+
+function MainPage() {
+    return (
+    <div className="main-page">
+      <h1>This is the Donut shops main page</h1>
+      <h1>Main Page</h1>
+    </div>
+  );
+}
+
+
+
+
+export default MainPage;
