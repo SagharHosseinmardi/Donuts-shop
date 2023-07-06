c85e9778095c4be4abc345515e031fab40a4abfc
Andriana <andrianasaffo@gmail.com>
2023-07-06 15:22:35 +0100

    Create donutInfo.js
    Utilities file added to hold information regarding donuts

---
+++ client/src/Utilities/donutInfo.js
@@ -0,0 +1,52 @@
+const donuts = [
+	{
+		id: 1,
+		imgSrc: "",
+		alt: "chocolate donut",
+		name: "Chocolate Crunch",
+		description: "I'm a Chocolate Crunch donut",
+		price: "£2.50",
+	},
+	{
+		id: 2,
+		imgSrc: "",
+		alt: "strawberry donut",
+		name: "Strawberry Sprinkles",
+		description: "I'm a Strawberry Sprinkles donut",
+		price: "£2.50",
+	},
+	{
+		id: 3,
+		imgSrc: "",
+		alt: "caramel donut",
+		name: "Caramel Glaze",
+		description: "I'm a Caramel Glaze donut",
+		price: "£2.50",
+	},
+	{
+		id: 4,
+		imgSrc: "",
+		alt: "rose donut",
+		name: "Rose Vanilla",
+		description: "I'm a Rose Vanilla donut",
+		price: "£2.50",
+	},
+	{
+		id: 5,
+		imgSrc: "",
+		alt: "cookies donut",
+		name: "Cookies & Cream",
+		description: "I'm a Cookies & Cream donut",
+		price: "£2.50",
+	},
+	{
+		id: 6,
+		imgSrc: "",
+		alt: "lemon donut",
+		name: "Lemon Frosting",
+		description: "I'm a Lemon Frosting donut",
+		price: "£2.50",
+	},
+];
+
+export default donuts;
