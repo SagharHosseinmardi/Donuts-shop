8f78c54b18fe3739e238c764f4ad8636b253fae6
Gayle Ngozi <gaylengozi@gmail.com>
2023-07-06 12:16:14 +0100

    browser

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,32 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import MainPage from "./pages/main-page/MainPage";
+
+
 
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
+	<div className="main">
+		<MainPage />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about/this/site" element={<About />} />
+		</Routes>
+	</div>
 );
 
 export default App;
+
+{/* <div className="App">
+	<BrowserRouter>
+		<Header />
+		<Routes>
+			<Route path="/" element={<FrontPage />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="/skills" element={<Skills />} />
+			<Route path="/jobs" element={<Jobs />} />
+		</Routes>
+	</BrowserRouter>
+	<Footer />
+</div>; */}
